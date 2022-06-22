d04e872804c3a1993bd42e882d305c135be7da61
Dhruv Metha <dhruv.metha@gmail.com>
2022-06-21 23:04:40 -0400

    added pipeline with all configs

--- legged_gym/legged_gym/envs/base/legged_robot_config.py
+++ legged_gym/legged_gym/envs/base/legged_robot_config.py
@@ -41,11 +41,11 @@ class LeggedRobotCfg(BaseConfig):
         episode_length_s = 20 # episode length in seconds
 
     class terrain:
-        mesh_type = 'trimesh' # "heightfield" # none, plane, heightfield or trimesh
+        mesh_type = 'plane' # "heightfield" # none, plane, heightfield or trimesh
         horizontal_scale = 0.1 # [m]
         vertical_scale = 0.005 # [m]
         border_size = 25 # [m]
-        curriculum = True
+        curriculum = False # True
         static_friction = 1.0
         dynamic_friction = 1.0
         restitution = 0.
@@ -165,7 +165,7 @@ class LeggedRobotCfg(BaseConfig):
         clip_actions = 100.
 
     class noise:
-        add_noise = True
+        add_noise = False # True
         noise_level = 1.0 # scales other values
         class noise_scales:
             dof_pos = 0.01
@@ -235,7 +235,7 @@ class LeggedRobotCfgPPO(BaseConfig):
         max_iterations = 1500 # number of policy updates
 
         # logging
-        save_interval = 50 # check for potential saves every this many iterations
+        save_interval = 500 # check for potential saves every this many iterations
         experiment_name = 'test'
         run_name = ''
         # load and resume
