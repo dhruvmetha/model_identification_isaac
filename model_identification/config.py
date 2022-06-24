d4193e35c8ce3aa210bdd6d43fe17c62db46ee6b
Dhruv Metha Ramesh <dm1487@rlab1.cs.rutgers.edu>
2022-06-24 11:45:07 -0400

    trying out the backflip policy

--- model_identification/config.py
+++ model_identification/config.py
@@ -2,15 +2,15 @@ from legged_gym.envs.base.base_config import BaseConfig
 from search_strategies_config import *
 
 class RunConfig(BaseConfig):
-    train_base: bool = False
+    train_base: bool = True
     collect_base: bool = True
-    search_model: bool = True
-    train_best_model: bool = True
-    collect_best_model: bool = True
-    record_error: bool = True
+    search_model: bool = False
+    train_best_model: bool = False
+    collect_best_model: bool = False
+    record_error: bool = False
     
-    trained_model: str = "plane_default_test" # location of trained model to be used
-    results_folder = "results"
+    trained_model: str = "plane_default_flip" # location of trained model to be used
+    results_folder = "results_flip"
     root_save_folder: str = f"{results_folder}/test"
     ground_truth_path: str = f"{root_save_folder}/ground_truth/gt.pkl"
     best_model_path: str = f"{root_save_folder}/best_model/model.pkl"
@@ -19,23 +19,53 @@ class RunConfig(BaseConfig):
     observation_path: str = f"{root_save_folder}/observation/obs.pkl"
 
 class TrainConfig(BaseConfig):
+
+    class env:
+        num_envs = 4096
     
     class domain_rand:
         randomize_friction = True
-        randomize_base_mass = True
+        # randomize_base_mass = True # walk
+        randomize_base_mass = False
         friction_range = [0.5, 1.25]
         added_mass_range = [-1., 1.]
-        push_robots = True
+        # push_robots = True  # walk
+        push_robots = False
     
     class noise:
-        add_noise = True
+        add_noise = True 
     
     class commands:
         class ranges:
-            lin_vel_x = [0., 1.0] # min max [m/s]
+            # lin_vel_x = [0., 1.0] # min max [m/s]
+            # lin_vel_y = [0., 0.]   # min max [m/s]
+            # ang_vel_yaw = [-1, 1]    # min max [rad/s]
+            # heading = [-3.14, 3.14]
+
+            lin_vel_x = [0., 0.] # min max [m/s]
             lin_vel_y = [0., 0.]   # min max [m/s]
-            ang_vel_yaw = [-1, 1]    # min max [rad/s]
-            heading = [-3.14, 3.14]
+            ang_vel_yaw = [-3.14., 3.14]    # min max [rad/s]
+            heading = [0., 0.]
+
+    class rewards:
+        class scales:
+            termination = -0.0
+            tracking_lin_vel = -0.0
+            tracking_ang_vel = 0.0
+            lin_vel_z = 1.0
+            ang_vel_xy = -1.0
+            orientation = -0.
+            torques = -0.0
+            dof_vel = -0.
+            dof_acc = -0.
+            base_height = -0.0 
+            feet_air_time =  2.0
+            collision = -1.0
+            feet_stumble = -0.0 
+            action_rate = -0.0
+            stand_still = -0.
+        
+        only_positive_rewards = False
 
 class SearchConfig(BaseConfig):
     strategies = dict(exhaustive=exhaustive(), random=random(), adaptive=adaptive(), cross_entropy=cross_entropy()) 
@@ -95,7 +125,7 @@ class CollectConfig(BaseConfig):
             class query_model:
                 ## ground truth definition
                 shapes = [1.4,] * 13 # friction values -> same as len(env.shapes)
-                bodies = [0.4, *([0.02] * 5), *([-0.1] * 2)]  # mass values -> same as len(env.bodies)
+                bodies = [0.4, *([0.002] * 5), *([-0.001] * 2)]  # mass values -> same as len(env.bodies)
                 
 
 
